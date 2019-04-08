b3bcb76654856664af3d55a827350c0fad855ea6
Marian Zburlea <marianzburlea@gmail.com>
2019-04-08 15:19:32 +0100

    add graphiql tool which we use only on the development server

--- api.js
+++ api.js
@@ -4,6 +4,10 @@ const expressGraphQL = require('express-graphql')
 const myApp = express()
 const port = 4000
 
+myApp.use('/graphql', expressGraphQL({
+  graphiql: true,
+}))
+
 myApp.listen(port, () => {
   console.log(`I'm listening on port ${port}.`)
 })
